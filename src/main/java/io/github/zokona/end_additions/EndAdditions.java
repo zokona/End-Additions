a6483341447cf70ea8652a826d2b2aca667660da
Kona <mfranchuk97@gmail.com>
2024-08-01 22:26:32 -0700

    add

--- src/main/java/io/github/zokona/end_additions/EndAdditions.java
+++ src/main/java/io/github/zokona/end_additions/EndAdditions.java
@@ -1,16 +1,8 @@
 package io.github.zokona.end_additions;
 
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.text.Text;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
@@ -27,7 +19,7 @@ public class EndAdditions implements ModInitializer {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
 
 		AttackEntityCallback.EVENT.register((player, world, hand, entity, result) -> {
-			System.out.println(player.getHandItems());
+			System.out.println(hand);
 
 			return ActionResult.SUCCESS;
 		});
