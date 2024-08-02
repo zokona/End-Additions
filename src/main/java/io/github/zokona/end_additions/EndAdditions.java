42f38a09a0e8868fd747f3891190b488ea438bf7
Kona <mfranchuk97@gmail.com>
2024-08-02 15:48:37 -0700

    Life Crystal
    finished

--- src/main/java/io/github/zokona/end_additions/EndAdditions.java
+++ src/main/java/io/github/zokona/end_additions/EndAdditions.java
@@ -1,7 +1,12 @@
 package io.github.zokona.end_additions;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
@@ -16,12 +21,14 @@ public class EndAdditions implements ModInitializer {
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		ServerLivingEntityEvents.ALLOW_DEATH.register((entity, damageSource, damageAmount) -> {
+			if (damageSource.getAttacker() instanceof ServerPlayerEntity) {
+				if (((ServerPlayerEntity)damageSource.getAttacker()).getInventory().offHand.get(0).getItem() == EndAdditionsItems.LIFE_CRYSTAL) {
+					LifeCrystal.heal((ServerPlayerEntity)damageSource.getAttacker());
+				}
+			}
 
-		AttackEntityCallback.EVENT.register((player, world, hand, entity, result) -> {
-			System.out.println(hand);
-
-			return ActionResult.SUCCESS;
+			return true;
 		});
 
 		EndAdditionsItems.register(mod);
--- src/main/java/io/github/zokona/end_additions/LifeCrystal.java
+++ src/main/java/io/github/zokona/end_additions/LifeCrystal.java
@@ -1,19 +1,28 @@
 package io.github.zokona.end_additions;
 
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.item.TooltipConfig;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.world.World;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.List;
 
 public class LifeCrystal extends Item {
 	public LifeCrystal(Settings settings) {
 		super(settings);
+	}
+
+	public static void heal(ServerPlayerEntity player) {
+		if (player.getHealth() < player.getMaxHealth()) {
+			player.setHealth(player.getHealth() + 3);
+		}
+	}
+
+	@Override
+	public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipConfig config) {
+		tooltip.add(Text.translatable("itemTooltip.end_additions.life_crystal").formatted(Formatting.WHITE));
 	}
 }
